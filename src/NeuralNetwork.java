fffbe01eab72862a64cada00b4666e1cb98c96ee
Haitam <haitam_benyahia@hotmail.com>
2016-11-08 12:19:20 +0100

    finished NeuralNetwork but without input

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,15 +1,89 @@
 import scr.SensorModel;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Random;
+
 
 public class NeuralNetwork implements Serializable {
 
     private static final long serialVersionUID = -88L;
-
+    private double[][]weights1;
+    private double[][]weights2;
+    private double[]bias1;
+    private double[]bias2;
+    private int numInputNeurons;
+    private int numHiddenNeurons;
+    private int numOutputNeurons;
+    private Random r = new Random();
+    
     NeuralNetwork(int inputs, int hidden, int outputs) {
+    	// This should be a parameter..
+    	double sigma = 1;
+    	double mean = 0;
+    	
+    	// initialize class variables
+    	weights1 = new double[hidden][inputs]; 
+    	weights2 = new double[outputs][hidden]; 
+    	bias1 = new double[hidden];
+    	bias2 = new double[outputs];
+    	numInputNeurons = inputs;
+    	numHiddenNeurons = hidden;
+    	numOutputNeurons = outputs;
+    	
+    	// initialize weights 1 with Gaussian distributed values with sigma and mean
+    	for (int i=0; i< hidden; i++){
+    		for (int j=0; j<inputs; j++){
+    			weights1[i][j] = r.nextGaussian() * sigma + mean;
+    		}
+    	}
+    	
+    	// initialize weights 2 with Gaussian distributed values with sigma and mean
+    	for (int i=0; i< outputs; i++){
+    		for (int j=0; j<hidden; j++){
+    			weights2[i][j] = r.nextGaussian() * sigma + mean; //different seed?
+    		}
+    	}
+    	
+    	// initialize bias with Gaussian distributed values with sigma and mean
+    	for (int i=0; i< hidden; i++){
+    		bias1[i] = r.nextGaussian() * sigma + mean; // different seed?
+    	}
+    	
+    	for (int i=0; i< outputs; i++){
+    		bias2[i] = r.nextGaussian() * sigma + mean; // different seed?
+    	}
     }
 
+    // Feed forward algorithm
     public double getOutput(SensorModel a) {
+    	// put SensorModel a in input
+    	
+    	//Initialize intermediate variables
+    	double[] input = new double[numInputNeurons];
+    	double[] h = new double[numHiddenNeurons];
+    	double[] output = new double[numInputNeurons];
+    	
+    	for (int i=0; i< numHiddenNeurons; i++){
+    		for (int j=0; j<numInputNeurons; j++){
+    			h[i] += input[j] * weights1[i][j];
+    		}
+    		
+    		double tempH = h[i]  +bias1[i]; //add bias
+    		h[i] = 1 / (1 + Math.exp(-tempH)); // Possibly use ReLU instead
+    	}
+    	
+    	for (int i=0; i< numOutputNeurons; i++){
+    		for (int j=0; j<numHiddenNeurons; j++){
+    			output[i] += h[j] * weights2[i][j];
+    		}
+    		
+    		double tempH = output[i] + bias2[i]; //add bias
+    		output[i] = 1 / (1 + Math.exp(-tempH)); // Possibly use ReLU instead
+    		System.out.println(output[i]);
+    	}
+    	
+    	//return output;
         return 0.5;
     }
 
